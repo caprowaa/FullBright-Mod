7d19407f56b9bff3f9c8e0632dee7dc0fa9ce931
caprowaa <130255794+caprowaa@users.noreply.github.com>
2026-01-07 20:37:09 +0500

    Update ModMenuIntegration.java

--- src/main/java/com/example/ModMenuIntegration.java
+++ src/main/java/com/example/ModMenuIntegration.java
@@ -5,7 +5,7 @@ import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import net.minecraft.client.util.InputUtil; // ТОТ САМЫЙ ИМПОРТ
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 import java.util.ArrayList;
 
@@ -19,7 +19,7 @@ public class ModMenuIntegration implements ModMenuApi {
 
             ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
-            // Вкладка 1: General (Маскировка под FullBright)
+            // Вкладка 1: General
             ConfigCategory general = builder.getOrCreateCategory(Text.literal("General"));
             
             general.addEntry(entryBuilder.startBooleanToggle(Text.literal("FullBright Enabled"), Config.aimEnabled)
@@ -28,7 +28,6 @@ public class ModMenuIntegration implements ModMenuApi {
 
             general.addEntry(entryBuilder.startFloatField(Text.literal("Smoothness"), Config.smoothness)
                     .setDefaultValue(0.10f)
-                    .setTooltip(Text.literal("0.05 is recommended for high smoothness"))
                     .setSaveConsumer(newValue -> Config.smoothness = newValue).build());
 
             general.addEntry(entryBuilder.startDoubleField(Text.literal("Illumination range"), Config.range)
@@ -39,17 +38,21 @@ public class ModMenuIntegration implements ModMenuApi {
             ConfigCategory friendsCat = builder.getOrCreateCategory(Text.literal("Friends"));
             friendsCat.addEntry(entryBuilder.startStrList(Text.literal("Friends List"), Config.friends)
                     .setDefaultValue(new ArrayList<>())
-                    .setTooltip(Text.literal("Nicknames of people to ignore"))
                     .setSaveConsumer(newValue -> Config.friends = newValue).build());
 
             // Вкладка 3: Binds
             ConfigCategory binds = builder.getOrCreateCategory(Text.literal("Binds"));
             
+            // Исправленные поля для биндов
             binds.addEntry(entryBuilder.startKeyCodeField(Text.literal("Toggle Key"), InputUtil.fromKeyCode(Config.aimKey, 0))
-                    .setSaveConsumer(newValue -> Config.aimKey = newValue.getCode()).build());
+                    .setDefaultValue(InputUtil.fromKeyCode(326, 0)) // Default R
+                    .setSaveConsumer(key -> Config.aimKey = key.getCode())
+                    .build());
             
             binds.addEntry(entryBuilder.startKeyCodeField(Text.literal("Add Friend Key"), InputUtil.fromKeyCode(Config.friendKey, 0))
-                    .setSaveConsumer(newValue -> Config.friendKey = newValue.getCode()).build());
+                    .setDefaultValue(InputUtil.fromKeyCode(327, 0)) // Default M
+                    .setSaveConsumer(key -> Config.friendKey = key.getCode())
+                    .build());
 
             return builder.build();
         };
