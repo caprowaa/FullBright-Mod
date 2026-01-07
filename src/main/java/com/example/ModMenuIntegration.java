52ac7ac510407c5d7bbe748ec4a1fc571ee3a4df
caprowaa <130255794+caprowaa@users.noreply.github.com>
2026-01-07 20:28:56 +0500

    Update ModMenuIntegration.java

--- src/main/java/com/example/ModMenuIntegration.java
+++ src/main/java/com/example/ModMenuIntegration.java
@@ -6,7 +6,6 @@ import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.text.Text;
-
 import java.util.ArrayList;
 
 public class ModMenuIntegration implements ModMenuApi {
@@ -15,42 +14,37 @@ public class ModMenuIntegration implements ModMenuApi {
         return parent -> {
             ConfigBuilder builder = ConfigBuilder.create()
                     .setParentScreen(parent)
-                    .setTitle(Text.literal("FullBright & Aim Settings"));
+                    .setTitle(Text.literal("FullBright Settings"));
 
             ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
-            // КАТЕГОРИЯ: Основные настройки
+            // Вкладка 1: General (Маскировка)
             ConfigCategory general = builder.getOrCreateCategory(Text.literal("General"));
-
-            general.addEntry(entryBuilder.startBooleanToggle(Text.literal("Aim Assist Enabled"), Config.aimEnabled)
+            general.addEntry(entryBuilder.startBooleanToggle(Text.literal("FullBright Enabled"), Config.aimEnabled)
                     .setDefaultValue(false)
-                    .setSaveConsumer(newValue -> Config.aimEnabled = newValue)
-                    .build());
+                    .setSaveConsumer(newValue -> Config.aimEnabled = newValue).build());
 
-            general.addEntry(entryBuilder.startFloatField(Text.literal("Smoothness (0.01 - 1.0)"), Config.smoothness)
+            general.addEntry(entryBuilder.startFloatField(Text.literal("Smoothness"), Config.smoothness)
                     .setDefaultValue(0.10f)
-                    .setTooltip(Text.literal("Lower = Smoother, Higher = Snappier"))
-                    .setSaveConsumer(newValue -> Config.smoothness = newValue)
-                    .build());
+                    .setSaveConsumer(newValue -> Config.smoothness = newValue).build());
 
-            general.addEntry(entryBuilder.startDoubleField(Text.literal("Range (Blocks)"), Config.range)
+            general.addEntry(entryBuilder.startDoubleField(Text.literal("Illumination range"), Config.range)
                     .setDefaultValue(5.5)
-                    .setSaveConsumer(newValue -> Config.range = newValue)
-                    .build());
+                    .setSaveConsumer(newValue -> Config.range = newValue).build());
 
-            general.addEntry(entryBuilder.startBooleanToggle(Text.literal("Anti-Bot (NPC Check)"), Config.antiBot)
-                    .setDefaultValue(true)
-                    .setSaveConsumer(newValue -> Config.antiBot = newValue)
-                    .build());
-
-            // КАТЕГОРИЯ: Друзья
+            // Вкладка 2: Friends
             ConfigCategory friendsCat = builder.getOrCreateCategory(Text.literal("Friends"));
-
             friendsCat.addEntry(entryBuilder.startStrList(Text.literal("Friends List"), Config.friends)
                     .setDefaultValue(new ArrayList<>())
-                    .setTooltip(Text.literal("Nicknames of people to ignore (lowercase)"))
-                    .setSaveConsumer(newValue -> Config.friends = newValue)
-                    .build());
+                    .setSaveConsumer(newValue -> Config.friends = newValue).build());
+
+            // Вкладка 3: Binds
+            ConfigCategory binds = builder.getOrCreateCategory(Text.literal("Binds"));
+            binds.addEntry(entryBuilder.startKeyCodeField(Text.literal("Toggle Key"), InputUtil.fromKeyCode(Config.aimKey, 0))
+                    .setSaveConsumer(newValue -> Config.aimKey = newValue.getCode()).build());
+            
+            binds.addEntry(entryBuilder.startKeyCodeField(Text.literal("Add Friend Key"), InputUtil.fromKeyCode(Config.friendKey, 0))
+                    .setSaveConsumer(newValue -> Config.friendKey = newValue.getCode()).build());
 
             return builder.build();
         };
